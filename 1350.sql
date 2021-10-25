320a25f653956d2b8e81c962a37ea537219ab5e0
Xuchen Sun <xuchens@mun.ca>
2021-10-25 15:54:02 -0230

    Update 1350.sql

--- 1350.sql
+++ 1350.sql
@@ -1,5 +1,6 @@
-#1350  delet  1,7,13 
-select Students.id,Students.name 
-from Students
 
-where Students.department_id !='1'and Students.department_id !='7' and Students.department_id !='13';
+#1350  not in
+SELECT id,name 
+FROM Students
+WHERE department_id 
+NOT IN(SELECT id FROM Departments);
