a58f710dff0a6c8919c1d92448f5d65bcf19b4a3
Xuchen Sun <xuchens@mun.ca>
2021-11-01 01:38:39 -0230

    Create 1929.java

---
+++ 1929.java
@@ -0,0 +1,15 @@
+//1929 Logic: use two loop to add int to the result array one by one
+class Solution {
+    public int[] getConcatenation(int[] nums) {
+        int[] result=new int[nums.length*2];
+        
+        for(int i=0;i<nums.length;i++){// first loop
+            result[i]=nums[i];
+        }
+          for(int j=0;j<nums.length;j++){//second loop
+            result[nums.length+j]=nums[j];
+        }
+        
+        return result;
+    }
+}
