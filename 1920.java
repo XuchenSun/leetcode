e350a23f9c607bd90e018e1f12823fb8ff4b97dc
Xuchen Sun <xuchens@mun.ca>
2021-11-01 01:27:38 -0230

    Create 1920.java

---
+++ 1920.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int[] buildArray(int[] nums) {
+        int[] result=new int[nums.length];// define a new array according to input nums
+        //System.out.print(nums.length);// check length
+        int current=0;//define a int to get the current index
+        for(int i=0;i<nums.length;i++){// use loop to get the result
+            current=nums[i];
+            result[i]=nums[current];
+        }
+        
+        
+        return result;
+    }
+}
